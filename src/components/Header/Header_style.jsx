c3c0167b1bae993fc4fe59e5c792f85b0780edd1
AlexandreFouher <foucher.alex@gmail.com>
2023-09-25 14:01:20 +0200

    add old project files

---
+++ src/components/Header/Header_style.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Header_Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: start;s
+`
+
+export const Header_Tittle_Container = styled.div`
+    width : 50%;
+    display: flex;
+    justify-content: center;
+`
+
+export const Header_Tittle = styled.h1`
+`
+
+export const Header_Burger_Container = styled.div`
+    width: 20%;
+    display: flex;
+    justify-content: center;
+`
