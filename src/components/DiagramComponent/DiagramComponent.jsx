a154b20a6cc658ff0c55179958c789dfe4b2b38f
AlexandreFouher <foucher.alex@gmail.com>
2023-09-28 11:40:23 +0200

    update :style flex diagram component & detailspage

--- src/components/DiagramComponent/DiagramComponent.jsx
+++ src/components/DiagramComponent/DiagramComponent.jsx
@@ -5,15 +5,18 @@ import './DiagramCompnent.css';
 const DiagramComponent = (props) => {
     return (
         <div className="component_container">
-            <p className="componant_name">
+            <h4 className="componant_name">
                 {props.name}
-            </p>
-            <img src={props.url} alt="component of diagram" className="component_img"/>
-            <p className="component_desc">
-                {props.description}
-            </p>
+            </h4>
+            <div className="component-desc-container">
+                <img src={props.url} alt="component of diagram" className="component_img" />
+                <p className="component_desc">
+                    {props.description}
+                </p>
+            </div>
+            <hr/>
         </div>
     )
 }
 
-export default DiagramComponent ;
+export default DiagramComponent;
